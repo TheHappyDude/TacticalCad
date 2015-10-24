bcc252672e6ae1042432fe8b024bb072ffe68157
kwang329 <kwang329@gatech.edu>
2015-10-24 17:42:03 -0400

    Updated about page, added an actual banner

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 //Contains Javascript and JQuery elements of the web page
 //Kaiwen Wang
 
-var nerfParts = 10;         //Find way to get server to update this in the future
-var fstrikeParts = 5;       //Same here
-var retalParts = 5;         //and here
+var nerfParts = 3;         //Find way to get server to update this in the future
+var fstrikeParts = 2;       //Same here
+var retalParts = 1;         //and here
 var protoParts = 5;         //and here too
-var univParts = 10;         //and here
+var univParts = 0;         //and here
 
 //Generates actions on web page load, which will be most of them
 $(document).ready(function(){
