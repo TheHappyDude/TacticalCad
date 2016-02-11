558a6584a6f20dda57fb020acb72f41fe0778e31
kwang329 <kwang329@gatech.edu>
2016-02-11 22:08:34 +0800

    Merged navbar and sidebar into scripts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//Contains Javascript and JQuery elements of the web page
+//Contains Javascript and JQuery elements of the web page, as well as common page elements
 //Kaiwen Wang
 
 var nerfParts = 5;         //Find way to get server to update this in the future
@@ -33,5 +33,54 @@ function printFooter() {
     sidebar += "<small>So this is normally where copyright information goes. Since I don't have any, here's a smiley face :D</small>"
     sidebar += "<br><small>Send questions/suggestions to tacticalcad@gmail.com</small>"
     sidebar += "<br><hr>"
+
+    document.write(sidebar);
+}
+
+function printNavbar() {
+    var navbar = "";
+    navbar += "<nav class=\"navbar navbar-custom\">";
+    navbar += "    <div class=\"container-fluid\">";
+    navbar += "        <div class=\"navbar-header\">";
+    navbar += "            <a class=\"navbar-brand\" href=\"\/TacticalCad\/index.html\">TacticalCAD<\/a>";
+    navbar += "        <\/div>";
+    navbar += "        <ul class=\"nav navbar-nav\">";
+    navbar += "            <li><a href=\"\/TacticalCad\/index.html\">Home<\/a><\/li>";
+    navbar += "            <li class=\"dropdown\">";
+    navbar += "                <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"\/TacticalCad\/lists\/nerf.html\">Nerf<span class=\"caret\"><\/span><\/a>";
+    navbar += "                <ul class=\"dropdown-menu\">";
+    navbar += "                    <li><a href=\"\/TacticalCad\/lists\/firestrike.html\">Firestrike<\/a><\/li>";
+    navbar += "                    <li><a href=\"\/TacticalCad\/lists\/retaliator.html\">Retaliator<\/a><\/li>";
+    navbar += "                    <li><a href=\"\/TacticalCad\/lists\/universal.html\">Universal<\/a><\/li>";
+    navbar += "                <\/ul>";
+    navbar += "            <\/li>";
+    navbar += "            <li><a href=\"\/TacticalCad\/lists\/prototypes.html\">Prototypes<\/a><\/li>";
+    navbar += "            <li><a href=\"\/TacticalCad\/about.html\">About<\/a><\/li>";
+    navbar += "        <\/ul>";
+    navbar += "    <\/div>";
+    navbar += "<\/nav>";
+
+    document.write(navbar);
+}
+
+function printSidebar() {
+    var sidebar="";
+    sidebar += "<div class=\"bs-sidebar affix\" id=\"sidebar\" style=\"background-color: orange;\">";
+    sidebar += "    <br>";
+    sidebar += "        <ul>";
+    sidebar += "            <li class=\"heading\"><a href=\"\/TacticalCad\/index.html\" id=\"hIndex\" class=\"unselected\">Home<\/a><\/li>";
+    sidebar += "            <li><hr><\/li>";
+    sidebar += "            <li class=\"heading\"><a href=\"\/TacticalCad\/lists\/nerf.html\" id=\"hNerf\" class=\"unselected\">Nerf Parts<\/a> (<span id=\"nerfCount\">0<\/span>)<\/li>";
+    sidebar += "            <li><a href=\"\/TacticalCad\/lists\/firestrike.html\" id=\"hFirestrike\" class=\"unselected\">Firestrike<\/a> (<span id=\"fstrikeCount\">0<\/span>)<\/li>";
+    sidebar += "            <li><a href=\"\/TacticalCad\/lists\/retaliator.html\" id=\"hRetaliator\" class=\"unselected\">Retaliator<\/a> (<span id=\"retalCount\">0<\/span>)<\/li>";
+    sidebar += "            <li><a href=\"\/TacticalCad\/lists\/universal.html\" id=\"hUniversal\" class=\"unselected\">Universal<\/a> (<span id=\"univCount\">0<\/span>)<\/li>";
+    sidebar += "            <li><hr><\/li>";
+    sidebar += "            <li class=\"heading\"><a href=\"\/TacticalCad\/lists\/prototypes.html\" id=\"hPrototypes\" class=\"unselected\">Prototypes<\/a> (<span id=\"protoCount\">0<\/span>)<\/li>";
+    sidebar += "            <li><hr><\/li>";
+    sidebar += "            <li class=\"heading\"><a href=\"\/TacticalCad\/about.html\" id=\"hAbout\" class=\"unselected\">About<\/a><\/li>";
+    sidebar += "        <\/ul>";
+    sidebar += "    <br>";
+    sidebar += "<\/div>";
+
     document.write(sidebar);
 }
